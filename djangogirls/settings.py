a395f2010ca4900ce422d861708656da06bfbf12
mustango <yu-sat@i.ua>
2017-11-21 14:14:05 +0200

    alowed hosts change

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+-5y#utmgh07=he!rn5#1g)rvo17268xj6w*18s+j=a6bgdwu8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://mustango.pythonanywhere.com']
 
 
 # Application definition
